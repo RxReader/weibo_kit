c4da70e1f32f958aeacae65543321a15a68b311d
v7lin <v7lin@qq.com>
2019-05-17 11:51:11 +0800

    xxx

--- lib/fake_weibo.dart
+++ lib/fake_weibo.dart
@@ -6,5 +6,4 @@ export 'src/domain/api/weibo_user_info_resp.dart'
 export 'src/domain/sdk/weibo_auth_resp.dart' hide WeiboAuthRespSerializer;
 export 'src/domain/sdk/weibo_sdk_resp.dart' hide WeiboSdkRespSerializer;
 export 'src/weibo.dart';
-export 'src/weibo_provider.dart';
 export 'src/weibo_scope.dart';
